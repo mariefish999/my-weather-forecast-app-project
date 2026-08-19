59e52249e185c25731ac2e9c68fd46b978d6acb6
mariefish999 <marie.fisher73@yahoo.co.uk>
2026-08-19 17:00:07 +0100

    added weather forecast html using javascript

--- src/javascript.js
+++ src/javascript.js
@@ -55,5 +55,36 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecast = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+        <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-icon">⛅</div>
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>15o</strong>
+            </div>
+            <div class="weather-forecast-temperature">
+              <strong>9o</strong>
+            </div>
+          </div>
+        </div>
+      `;
+  });
+
+  forecast.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+displayForecast();
